673178bc4d8419aade5db6eb549e54e359c3070a
George Fedorov <george.fedorov@playrix.com>
2022-09-02 22:23:08 +0200

    Reminder to fix parsing at the morning

--- tests/main.cpp
+++ tests/main.cpp
@@ -13,24 +13,13 @@
 int main(int argc, char** argv) {
 
 	ebnf::Ebnf form(
-		"non-zero-digit='1'|'2'|'3'|'4'|'5'|'6'|'7'|'8'|'9'."
-		"zero='0'."
-		"digit=zero|non-zero-digit."
-		"integer$=non-zero-digit{digit}."
-		"float-number$=[non-zero-digit{digit}|zero]'.'{digit}."
-		"number=integer|float-number."
-
-		"space=' '."
-		"expression={space}equality-expression{space}."
-		"equality-expression=additive-expression{{space}('=='|'!='){space}additive-expression}."
-		"additive-expression=multiplicative-expression{{space}('+'|'-'){space}multiplicative-expression}."
-		"multiplicative-expression=primary{{space}('*'|'/'){space}primary}."
-		"primary='('expression')'|number|'-'primary."
+		"start='1'|'12'."
+		"end='23'."
+		"total=start end."
 	);
 
-	auto token = form.parseAs("  65 ==     7 - 420.69 * 35 + 6", "expression");
-	token.second.compress(form);
-	std::cout << token.second.toStr() << std::endl;
+	auto [success, token] = form.parseAs("123", "total"); //TODO
+	std::cout << std::boolalpha << success << " " << token.toStr() << std::endl;
 
 	getc(stdin);
 	return 0;
